877b7e3d5d5ad4803e81257ee15b35be02612044
Aber72 <abhdutta356@gmail.com>
2024-05-16 18:05:21 +0530

    2nd commit

--- Print.java
+++ Print.java
@@ -2,5 +2,6 @@ class Print{
     public static void main(String[] args){
         System.out.println("Hello New Day");
         System.out.println("Hello New Begining");
+        System.out.println("Hello New Begining to u");
     }
 }
